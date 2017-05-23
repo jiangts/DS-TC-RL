fd111eedcc40a4fbccdf5ee6fd7089dc78505b65
LI DENG <dengl11@foxmail.com>
2017-05-23 11:54:11 -0700

    add comment

--- parser/index.js
+++ parser/index.js
@@ -2,17 +2,24 @@
 
 const parser = require('./parser');
 
-const argv = require('yargs')
-.help()
-.argv;
+const argv = require('yargs').help().argv;
 
 const fs = require('fs');
 
-if(argv.html)
-parser.parse({html: argv.html});
+/***** Parsing HTML *****/
+if(argv.html) {
+	parser.parse({html: argv.html});
+}
+
+/***** Parsing File *****/
 if(argv.file) {
   html = fs.readFileSync(argv.file, 'utf8');
   parser.parse({html});
 }
-if(argv._[0])
-parser.parse({url: argv._[0]});
+
+/***** Parsing URL *****/
+if(argv._[0] || argv.url) {
+	url = argv.url || argv._[0]
+	parser.parse({url: url});
+	console.log("URL Parsed: ", url)
+}
