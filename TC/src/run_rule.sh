88c1b0f58aa929501f5f76a6d79f548eee3840f5
Allan Jiang <origami789@gmail.com>
2017-06-02 15:59:29 -0700

    revert to vanilla TC-Bot

--- TC/src/run_rule.sh
+++
@@ -1,8 +0,0 @@
-python run.py --agt 5 --usr 1 --max_turn 40\
-          --episodes 150\
-                  --movie_kb_path ./deep_dialog/data/movie_kb.1k.p\
-                          --goal_file_path ./deep_dialog/data/user_goals_first_turn_template.part.movie.v1.p\
-                                  --intent_err_prob 0.00\
-                                          --slot_err_prob 0.00\
-                                                  --episodes 500\
-                                                          --act_level 0\
