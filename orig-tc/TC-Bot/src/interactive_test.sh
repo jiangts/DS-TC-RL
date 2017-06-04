8c7dcf00457d4513ca61b73c1a5db050451b6d08
Dan Iter <daniter@stanford.edu>
2017-06-04 14:56:54 -0700

    adding clean version of tc-bot for work

---
+++ orig-tc/TC-Bot/src/interactive_test.sh
@@ -0,0 +1,1 @@
+python run.py --agt 0 --usr 2 --max_turn 40 --episodes 150 --movie_kb_path ./deep_dialog/data/movie_kb.1k.p --goal_file_path ./deep_dialog/data/goals.json.p --intent_err_prob 0.00 --slot_err_prob 0.00 --episodes 500 --act_level 0 --run_mode 0 --cmd_input_mode 0
