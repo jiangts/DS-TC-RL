af85ecee92b5d19ff112a1d3032333edbdaf3d4b
Allan Jiang <origami789@gmail.com>
2017-06-09 22:58:51 -0700

    IT WORKS

--- orig-tc/TC-Bot/src/deep_dialog/agents/agent_dqn.py
+++ orig-tc/TC-Bot/src/deep_dialog/agents/agent_dqn.py
@@ -32,65 +32,66 @@ class AgentDQN(Agent):
         self.slot_set = slot_set
         self.act_cardinality = len(act_set.keys())
         self.slot_cardinality = len(slot_set.keys())
-        
+
         self.feasible_actions = dialog_config.feasible_actions
         self.num_actions = len(self.feasible_actions)
-        
+
         self.epsilon = params['epsilon']
         self.agent_run_mode = params['agent_run_mode']
         self.agent_act_level = params['agent_act_level']
         self.experience_replay_pool = [] #experience replay pool <s_t, a_t, r_t, s_t+1>
-        
+
         self.experience_replay_pool_size = params.get('experience_replay_pool_size', 1000)
         self.hidden_size = params.get('dqn_hidden_size', 60)
         self.gamma = params.get('gamma', 0.9)
         self.predict_mode = params.get('predict_mode', False)
         self.warm_start = params.get('warm_start', 0)
-        
+
         self.max_turn = params['max_turn'] + 4
         self.state_dimension = 2 * self.act_cardinality + 7 * self.slot_cardinality + 3 + self.max_turn
-        
+
         self.dqn = DQN(self.state_dimension, self.hidden_size, self.num_actions)
         self.clone_dqn = copy.deepcopy(self.dqn)
-        
+
         self.cur_bellman_err = 0
-                
+
         # Prediction Mode: load trained DQN model
         if params['trained_model_path'] != None:
             self.dqn.model = copy.deepcopy(self.load_trained_DQN(params['trained_model_path']))
             self.clone_dqn = copy.deepcopy(self.dqn)
             self.predict_mode = True
             self.warm_start = 2
-            
-    def clear_exp_pool(self): 
-        self.experience_replay_pool = [] #experience replay pool <s_t, a_t, r_t, s_t+1>  
 
-            
+    def clear_exp_pool(self):
+        self.experience_replay_pool = [] #experience replay pool <s_t, a_t, r_t, s_t+1>
+
+
     def initialize_episode(self):
         """ Initialize a new episode. This function is called every time a new episode is run. """
-        
+
         self.current_slot_id = 0
         self.phase = 0
-        self.request_set = ['moviename', 'starttime', 'city', 'date', 'theater', 'numberofpeople']
-    
-    
+        self.request_set = ['destination1', 'flightDate2', 'flightDate1', 'origin1', 'travelers']
+        # ['moviename', 'starttime', 'city', 'date', 'theater', 'numberofpeople']
+
+
     def state_to_action(self, state):
         """ DQN: Input state, output action """
-        
+
         self.representation = self.prepare_state_representation(state)
         self.action = self.run_policy(self.representation)
         act_slot_response = copy.deepcopy(self.feasible_actions[self.action])
         return {'act_slot_response': act_slot_response, 'act_slot_value_response': None}
-        
-    
+
+
     def prepare_state_representation(self, state):
         """ Create the representation for each state """
-        
+
         user_action = state['user_action']
         current_slots = state['current_slots']
         kb_results_dict = state['kb_results_dict']
         agent_last = state['agent_action']
-        
+
         ########################################################################
         #   Create one-hot of acts to represent the current user action
         ########################################################################
@@ -108,6 +109,8 @@ class AgentDQN(Agent):
         #   Create bag of request slots representation to represent the current user action
         ########################################################################
         user_request_slots_rep = np.zeros((1, self.slot_cardinality))
+        # print "USER ACTION", user_action
+        # print "USER REQUEST SLOTS", user_request_slots_rep
         for slot in user_action['request_slots'].keys():
             user_request_slots_rep[0, self.slot_set[slot]] = 1.0
 
@@ -140,7 +143,7 @@ class AgentDQN(Agent):
         if agent_last:
             for slot in agent_last['request_slots'].keys():
                 agent_request_slots_rep[0,self.slot_set[slot]] = 1.0
-        
+
         turn_rep = np.zeros((1,1)) + state['turn'] / 10.
 
         ########################################################################
@@ -167,10 +170,10 @@ class AgentDQN(Agent):
 
         self.final_representation = np.hstack([user_act_rep, user_inform_slots_rep, user_request_slots_rep, agent_act_rep, agent_inform_slots_rep, agent_request_slots_rep, current_slots_rep, turn_rep, turn_onehot_rep, kb_binary_rep, kb_count_rep])
         return self.final_representation
-      
+
     def run_policy(self, representation):
         """ epsilon-greedy policy """
-        
+
         if random.random() < self.epsilon:
             return random.randint(0, self.num_actions - 1)
         else:
@@ -180,12 +183,13 @@ class AgentDQN(Agent):
                 return self.rule_policy()
             else:
                 return self.dqn.predict(representation, {}, predict_model=True)
-    
+
     def rule_policy(self):
         """ Rule Policy """
-        
+
         if self.current_slot_id < len(self.request_set):
             slot = self.request_set[self.current_slot_id]
+            print "req set", self.request_set
             self.current_slot_id += 1
 
             act_slot_response = {}
@@ -197,72 +201,72 @@ class AgentDQN(Agent):
             self.phase += 1
         elif self.phase == 1:
             act_slot_response = {'diaact': "thanks", 'inform_slots': {}, 'request_slots': {} }
-                
+
         return self.action_index(act_slot_response)
-    
+
     def action_index(self, act_slot_response):
         """ Return the index of action """
-        
+
         for (i, action) in enumerate(self.feasible_actions):
             if act_slot_response == action:
                 return i
         print act_slot_response
         raise Exception("action index not found")
         return None
-    
-    
+
+
     def register_experience_replay_tuple(self, s_t, a_t, reward, s_tplus1, episode_over):
         """ Register feedback from the environment, to be stored as future training data """
-        
+
         state_t_rep = self.prepare_state_representation(s_t)
         action_t = self.action
         reward_t = reward
         state_tplus1_rep = self.prepare_state_representation(s_tplus1)
         training_example = (state_t_rep, action_t, reward_t, state_tplus1_rep, episode_over)
-        
+
         if self.predict_mode == False: # Training Mode
             if self.warm_start == 1:
                 self.experience_replay_pool.append(training_example)
         else: # Prediction Mode
             self.experience_replay_pool.append(training_example)
-    
+
     def train(self, batch_size=1, num_batches=100):
         """ Train DQN with experience replay """
-        
+
         for iter_batch in range(num_batches):
             self.cur_bellman_err = 0
             for iter in range(len(self.experience_replay_pool)/(batch_size)):
                 batch = [random.choice(self.experience_replay_pool) for i in xrange(batch_size)]
                 batch_struct = self.dqn.singleBatch(batch, {'gamma': self.gamma}, self.clone_dqn)
                 self.cur_bellman_err += batch_struct['cost']['total_cost']
-            
+
             print ("cur bellman err %.4f, experience replay pool %s" % (float(self.cur_bellman_err)/len(self.experience_replay_pool), len(self.experience_replay_pool)))
-            
-            
+
+
     ################################################################################
     #    Debug Functions
     ################################################################################
     def save_experience_replay_to_file(self, path):
         """ Save the experience replay pool to a file """
-        
+
         try:
             pickle.dump(self.experience_replay_pool, open(path, "wb"))
             print 'saved model in %s' % (path, )
         except Exception, e:
             print 'Error: Writing model fails: %s' % (path, )
-            print e         
-    
+            print e
+
     def load_experience_replay_from_file(self, path):
         """ Load the experience replay pool from a file"""
-        
+
         self.experience_replay_pool = pickle.load(open(path, 'rb'))
-    
-             
+
+
     def load_trained_DQN(self, path):
         """ Load the trained DQN from a file """
-        
+
         trained_file = pickle.load(open(path, 'rb'))
         model = trained_file['model']
-        
+
         print "trained DQN Parameters:", json.dumps(trained_file['params'], indent=2)
         return model
